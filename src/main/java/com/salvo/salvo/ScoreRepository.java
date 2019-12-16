8a5e9d9ef4588c4e8a69eca9acdd719f20c5e7be
Victor Asmad <victorasmad2@gmail.com>
2019-12-15 23:29:10 -0300

    ULTIMOS CAMBIOS

--- src/main/java/com/salvo/salvo/ScoreRepository.java
+++
@@ -1,6 +0,0 @@
-package com.salvo.salvo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-}
--- src/main/java/com/salvo/salvo/ShipRepository.java
+++
@@ -1,6 +0,0 @@
-package com.salvo.salvo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipRepository extends JpaRepository<Ship, Long> {
-}
